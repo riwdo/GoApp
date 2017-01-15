38fa6afa613b5f1c17f50fc90e49a08043e0c63c
riwdo <racso.hansson@gmail.com>
2017-01-15 18:54:47 +0100

    Created AsyncTask to retrieve data

--- app/src/main/java/com/example/oscar/goapp/MainActivity.java
+++ app/src/main/java/com/example/oscar/goapp/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.oscar.goapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView totalTextView;
+    EditText percentageTxt;
+    EditText numberTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        percentageTxt = (EditText) findViewById(R.id.percentageTxt);
+        numberTxt = (EditText) findViewById(R.id.numberTxt);
+
+        Button calcBtn = (Button) findViewById(R.id.calcBtn);
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float percentage = Float.parseFloat(percentageTxt.getText().toString());
+                float decimal = percentage / 100;
+                Log.d("Button", "button here");
+                try {
+                    Log.d("try", "try/catch");
+                    sendGet();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                float total = decimal * Float.parseFloat(numberTxt.getText().toString());
+                totalTextView.setText(Float.toString(total));
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void sendGet() throws Exception{
+
+        Log.d("sendGet", "send/get");
+        String url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=ols&format=json";
+
+        new RetrieveData().execute(url);
+
+
     }
 }
